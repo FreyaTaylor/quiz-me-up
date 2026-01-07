9fdc3fcdcbd007cb8c1a696455bb6dbdd154f7cd
LittleFlyers <1214519210@qq.com>
2026-01-07 18:11:18 +0800

    init

---
+++ src/main/java/com/example/quizmeup/QuizMeUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizmeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMeUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizMeUpApplication.class, args);
+	}
+
+}
